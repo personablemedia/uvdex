f7e5be793423563e7036c1a7c3d44d86da069873
cannapages <cannapages@gmail.com>
2014-07-28 17:20:36 -0600

    Vehicle Fetch buttom

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,7 @@
 class VehiclesController < ApplicationController
+	before_action :authenticate_user!
+	def update
+		system("rake vehicles:fetch &")
+		redirect_to "/admin", notice: "Vehicles are being updated they should be complete in about 10 minuets"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ UvdV1::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 	root to: "pages#home"
+
+	resources :vehicles do
+		collection do
+			get "update"
+		end
+	end
+
 	namespace :api do
 		namespace :v1 do
 			resources :vehicles, only: [:index, :show]
